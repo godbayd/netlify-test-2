27f40a82047c9bb37b96b16c83c84e7ca5557eda
Dustin Godbay <godbayd@aol.com>
2019-01-12 14:35:58 -0500

    ...

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import React from 'react';
+
 import {
   Link,
   Route,
   Switch
 } from 'react-router-dom';
+
 import {
   TransitionGroup,
-  CSSTransition
+  Transition
 } from 'react-transition-group';
 
-import './styles.css';
+import { TweenMax, Power4 } from 'gsap';
 
+/*
+  unlike CSSTransition, Transition works better with jsx inline styles
+*/
 
-const Home = (props) => <div className="page" style={{backgroundColor: 'lightblue'}}></div>
-const Work = (props) => <div className="page" style={{backgroundColor: 'orange'}}></div>
-const About = (props) => <div className="page" style={{backgroundColor: 'lime'}}></div>
+const pageStyle = (bgcolor) => ({
+  position: 'absolute',
+  width: '100%',
+  height: '100%',
+  backgroundColor: bgcolor,
+  transform: 'translateX(-100%)'
+});
+
+const Home = (props) => <div className="page" style={{...pageStyle('lightblue')}}>Home</div>
+const Work = (props) => <div className="page" style={{...pageStyle('orange')}}>Work</div>
+const About = (props) => <div className="page" style={{...pageStyle('lime')}}>About</div>
 
 class Nav extends React.Component {
   constructor(props){ super(props) };
@@ -45,18 +58,45 @@ class Main extends React.Component {
           ({location}) => {
               return (
                 <TransitionGroup component={null}>
-                  <CSSTransition
+                  <Transition
+                    in={true}
+                    timeout={600}
                     key={location.key}
-                    timeout={1000}
-                    classNames="fade"
-                    appear
+                    onEnter={
+                      (e) => {
+                        console.log(e)
+                        TweenMax.to(
+                          e, 0.3,
+                          {
+                            x: 0,
+                            opacity: 1,
+                            ease: Power4.easeOut
+                          }
+                        ).delay(0.3)
+                      }
+                    }
+                    onExit={
+                      (e) => {
+                        console.log(e)
+                        TweenMax.to(
+                          e, 0.3,
+                          {
+                            x: '-100%',
+                            opacity: 0,
+                            ease: Power4.easeIn
+                          }
+                        );
+                      }
+                    }
+                    mountOnEnter
+                    unmountOnExit
                   >
                     <Switch location={location}>
-                      <Route exact path="/" component={Home} />
-                      <Route exact path="/work" component={Work} />
-                      <Route exact path="/about" component={About} />
+                      <Route exact path="/" component={()=><Home />} />
+                      <Route exact path="/work" component={()=><Work />} />
+                      <Route exact path="/about" component={()=><About />} />
                     </Switch>
-                  </CSSTransition>
+                  </Transition>
                 </TransitionGroup>
               );
             }
